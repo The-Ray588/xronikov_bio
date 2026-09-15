9d66f66d56f68843cf3c167cb81d4e987d2a27d4
TheRay <v75407043@gmail.com>
2026-09-15 22:30:04 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  channelName: "хроников",
+  channelUrl: "https://t.me/defdimasik",
+  adminUrl: "https://t.me/xronikov",
+  adminName: "@xronikov",
+  subscribers: 6440,
+  tagline: "Истории, мемы и атмосфера",
+  avatar: "assets/avatar.jpg",
+  favicon: "assets/favicon.png",
+  nftMessage: "Хочу купить NFT",
+  copyLink: "https://t.me/defdimasik"
+};
