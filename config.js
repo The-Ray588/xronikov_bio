e1a72bc630fc0631fcf486f7b0c97512fa8ffc23
TheRay <v75407043@gmail.com>
2026-09-15 23:25:25 +0300

    Update tagline in config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   adminUrl: "https://t.me/xronikov",
   adminName: "@xronikov",
   subscribers: 6440,
-  tagline: "Истории, мемы и атмосфера",
+  tagline: "Истории, подарки и атмосфера",
   avatar: "assets/avatar.jpg",
   favicon: "assets/favicon.png",
   nftMessage: "Хочу купить NFT",
